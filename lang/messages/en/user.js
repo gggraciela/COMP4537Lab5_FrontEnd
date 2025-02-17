87dd8e074e81e41cc67a6f4f07b7893a89c029f6
Graciela <ariwijayagraciela@gmail.com>
2025-02-16 20:59:49 -0800

    added user.js

---
+++ lang/messages/en/user.js
@@ -0,0 +1,11 @@
+const messages = {
+  insertSuccess: 'Inserted successfully',
+  selectOnly: 'Only SELECT queries allowed',
+  invalidRequest: 'Invalid request',
+  insertError: 'Error inserting data',
+  queryError: 'Error executing query',
+  emptyQuery: 'SQL query cannot be empty',
+  backendError: 'An error occurred on the server',
+};
+
+export default messages;
